3a2278c8d32540041de14f695751ffcc3a41011c
CK Helmer <caitlinhelmer@gmail.com>
2019-06-07 20:53:51 -0400

    added radii and color-mapping to circles

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,36 +1,52 @@
+
+
 //Set up API call to GeoJSON
 var url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson';
 
+function getColor(d) {
+  return d > 8 ? '#ff4500' :
+        d > 6 ? '#ff7946' :
+        d > 4  ? '#ffa47d' :
+        d > 2   ? '#ffcdb4' :
+                  '#fff5ee';
+}
+
+
 d3.json(url, function(data) {
-  console.log(data.features[1].properties.mag)
-  var features = data.features
 
-  var properties = [];
-
-  for (var i = 0; i < features.length; i++) {
-    properties.push(features[i].properties);
-  }
-
-  var magnitudes = [];
-
-  // for (var j = 0; j < properties.length; j++) {
-  //   magnitudes.push(properties.mag)
-  // } 
-
-  console.log(properties);
-  
-  L.geoJSON(data, {
+  var earthquakes = L.geoJSON(data, {
     pointToLayer: function (feature, latlng) {
-      return L.circleMarker(latlng, {
-        radius : function() {
-          //Return magnitude for each
-        }
-      });
-    }
-  }).addTo(map)
+      var magnitude = feature.properties.mag;
+      
+      var circleMarkerOptions = {
+        radius : ((magnitude*1.4) ** 2),
+        fillColor: getColor(magnitude),
+        color: '#999',
+        fillOpacity: .9,
+        weight: .5
+      }
+      return L.circleMarker(latlng, circleMarkerOptions) //{
+
+        },
+      onEachFeature: function(feature, layer) {
+        
+        layer.bindTooltip(feature.properties.place, feature.properties.mag);
+      }
+      }  
+      );
+  
+  // var tectonicPlates = L.geoJSON(data, {
+  //   pointToLayer: function(feature, coordinates) {
+  //     var plates = coordinates
+  //     return L.polygon(plates)
+  //   }
+  // })    
+    createMap(earthquakes);  
+ 
 
 
-function createMap(earthquakes) {
+
+function createMap(earthquakes, tectonicPlates) {
   // Define streetmap and darkmap layers
   var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
@@ -70,17 +86,28 @@ function createMap(earthquakes) {
   var map = L.map('map-id', {
     center: [39.5, -98.35],
     zoom: 4,
-    layers: [lightmap, earthquakes]
+    layers: [lightmap, earthquakes, tectonicPlates]
   });
  
   //Create a layer control 
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(map)
+
+  // var legend = L.control({position: 'bottomright'});
+  //   legend.onAdd = function (map) {
+  //     var div = L.DomUtil.create('div', 'info legend'),
+  //     grades = [0, 1, 2, 3, 4, 5, 6, 7],
+  //     labels = [];
+
+  //     for (var i = 0; grades.length; i++) {
+  //       div.innerHTML += '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' + grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+  //       }
+  //       return div;
+  //   };
+       
+  // legend.addTo(map);
+  earthquakes.addTo(map);
+  
 };
-
-
-
-createMap();
-
-});
+})
