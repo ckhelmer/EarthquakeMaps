2f0d697c2842163131ec8be48e2bd0b27f55b5d1
CK Helmer <caitlinhelmer@gmail.com>
2019-06-10 20:58:49 -0400

    legend

--- static/js/logic.js
+++ static/js/logic.js
@@ -105,5 +105,30 @@ function createMap(earthquakes) {
 
   earthquakes.addTo(map);
   tectonicPlates.addTo(map);
+
+  // Create a legend to display information about our map
+  var legend = L.control({position: 'bottomright'});
+
+  legend.onAdd = function (map) {
+  
+      var div = L.DomUtil.create('div', 'info legend'),
+      grades = [0, 1, 2, 3, 4, 5, 6, 7, 8],
+      labels = [];
+
+      div.innerHTML+='Magnitude<br><hr>'
+  
+      // loop through our density intervals and generate a label with a colored square for each interval
+      for (var i = 0; i < grades.length; i++) {
+          div.innerHTML +=
+              '<i style="background:' + getColor(grades[i] + 1) + '">&nbsp&nbsp&nbsp&nbsp</i> ' +
+              grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+  }
+  
+  return div;
+  };
+  
+  legend.addTo(myMap);
+
+
 };
 })
